5e02cfad989083add0118f615c71d32040d18439
francismacabunga <francisnormanmacabunga@gmail.com>
2020-07-31 02:39:05 +0800

    First ever commit!

--- SwiftDemo/ViewController.swift
+++ SwiftDemo/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func addController() {
+        // This will add some controller!
+    }
 
 
 }
